405e77572fa0507345e762d427ac346fb7a70b0f
Gucastilho <gucastilho23@gmail.com>
2023-09-18 21:58:19 -0300

    typeScript sem funcionar

--- src/app/footer/footer.component.ts
+++ src/app/footer/footer.component.ts
@@ -8,3 +8,81 @@ import { Component } from '@angular/core';
 export class FooterComponent {
 
 }
+
+export class VendasComponent {
+  BebidasEspeciais = [
+    { produto: 'Vodka Mango', preco: 'R$189.00', detalhes: 'A Absolut Vodka sabor manga é uma deliciosa e refrescante variação da clássica vodka Absolut.'},
+    { produto: 'Vodka Absolute Pear', preco: 'R$199.00', detalhes: 'A Absolut Vodka sabor pera é uma variante encantadora e sofisticada da famosa vodka Absolut' },
+    { produto: 'Vodka Stolichnaya Premium', preco: 'R$20.000', detalhes: 'A Vodka Stolichnaya Premium é uma vodka russa tradicionalmente produzida, conhecida por sua pureza e qualidade inigualáveis' },
+  ];
+
+  // Estrutura de dados para marcas, modelos e preços de celulares
+  Bebidas = [
+    {
+      marca: 'Gin',
+      tipos: [
+        { nome: 'Gin clássico', preco: 'R$ 149.99' },
+        { nome: 'Gin cítrico', preco: 'R$169.99' },
+        { nome: 'Gin floral', preco: 'R$199.99' },
+        { nome: 'Herbal', preco: 'R$219.99' },
+
+      ]
+    },
+    {
+      marca: ' Whisky',
+      tipos: [
+        { nome: 'Jack Daniels', preco: 'R$299.99' },
+        { nome: 'Singleton Of Dufftown ', preco: 'R$149.99' },
+        { nome: 'Chivas Regal ', preco: 'R$249.99' },
+        { nome: 'Johnnie Walker Red Label', preco: 'R$199.99' },
+
+      ]
+    },
+    {
+      marca: 'Vodka',
+      tipos: [
+        { nome: 'Absolut', preco: 'R$229.99' },
+        { nome: 'Grey Goose', preco: 'R$299.99' },
+        { nome: 'Ciroc ', preco: 'R$189.99' },
+        { nome: 'Sminorff', preco: 'R$59.00' },
+
+      ]
+    },
+
+  ];
+
+
+  MarcaSelecionada: any;
+  ModeloSelecionado: any;
+  modelos: any;
+
+
+  updateListaModelo() {
+    // Lógica para atualizar a lista de modelos com base na marca selecionada
+    const MarcaSelecionada = this.MarcaSelecionada;
+    if (MarcaSelecionada) {
+      this.modelos = MarcaSelecionada.modelos;
+    } else {
+      this.modelos = [];
+    }
+  }
+
+
+  DesejoUsuario: string = '';
+  desejos: string[] = []; // Array para armazenar os desejos dos clientes
+
+  AdicionarDesejo() {
+    if (this.DesejoUsuario.trim() !== '') {
+      this.desejos.push(this.DesejoUsuario);
+      this.DesejoUsuario = '';
+    }
+  }
+  LimparListaDesejo() {
+    this.DesejoUsuario = ''; // Limpa o campo de entrada
+  }
+
+  LimparDesejos() {
+    this.desejos = []; // Limpa a lista de desejos
+  }
+}
+
